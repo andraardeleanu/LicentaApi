b92bd10c8b2d299a0c93ad87a1b0ba696df7beb7
avasiloaiecatalin <48545737+avasiloaiecatalin@users.noreply.github.com>
2024-04-02 18:02:54 +0300

    Update OrderFilterRequest.cs

--- Api2/Requests/OrderFilterRequest.cs
+++ Api2/Requests/OrderFilterRequest.cs
@@ -2,7 +2,7 @@
 {
     public class OrderFilterRequest
     {
-        public Guid? OrderNo { get; set; }
+        public string? OrderNo { get; set; }
         public string? CreatedBy { get; set; }
         public int? WorkPointId { get; set; }
         public string? Status { get; set; }
